0a3b1b5f35d566f21089ca71d0825a8c0cd4d938
akilinjamam <akilinjamam@gmail.com>
2024-06-18 03:33:17 +0600

    file-updated

--- src/components/dashboard/dashbord_titlebar/DashboardTitleBar.jsx
+++ src/components/dashboard/dashbord_titlebar/DashboardTitleBar.jsx
@@ -1,4 +1,5 @@
 // import { useEffect, useState } from 'react';
+
 import useHome from '../home/useHome';
 import dashboardTitle from './DashboardTitleBar.module.scss';
 const DashboardTitleBar = () => {
@@ -11,8 +12,14 @@ const DashboardTitleBar = () => {
     //     minute:'',
     //     second: ''
     // });
-    const {location} = useHome();
-  
+    const {location, navigate} = useHome();
+    
+    const mappingRoutes = location.split('/').slice(1)
+
+    const handleRoute = (index) => {
+        const findNavigation = mappingRoutes?.slice(0, index).join('/');
+        navigate(`/${findNavigation}`)
+     }
     // useEffect(() => {
     //     const daysOfWeek = [
     //         'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
@@ -31,7 +38,11 @@ const DashboardTitleBar = () => {
         <div className={`${dashboardTitle.main}`}>
            <div className={`${dashboardTitle.titleBar} flex_center `}>
                 <div className={`${dashboardTitle.titleBarContainer} flex_between`}>
-                     <p>{location.toUpperCase().replace(/\//g, " > ").replace(/_/g, ' ')}</p>
+                     <div className={`${dashboardTitle.mappedRoute}`}>
+                     {
+                        mappingRoutes?.map((route, index) => <span onClick={() => handleRoute(index+1)} key={index}><i className="uil uil-angle-right"></i>  {route.toUpperCase().replace(/_/g, ' ')} </span> )
+                     }
+                     </div>
                     {/* <p>{date.day}, {date.monthDate}  {date.monthName} {date.year}, {date.hour > 12 ? (date.hour - 12): date.hour === 0 ? 12 : date.hour}:{date.minute + 1}:{date.second < 10 ? ('0'+date.second) : date.second}  {date.hour >= 12 ? 'PM' : 'AM' }</p>  */}
                 </div>
             </div>  
