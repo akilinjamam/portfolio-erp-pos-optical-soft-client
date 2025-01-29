cfb15457667e788583136fff376915344ac17a15
akilinjamam <akilinjamam@gmail.com>
2025-01-29 11:08:49 +0600

    file-updated

--- src/components/dashboard/administration_module/manage_sales/ManageSales.jsx
+++ src/components/dashboard/administration_module/manage_sales/ManageSales.jsx
@@ -112,28 +112,28 @@ const ManageSales = () => {
                   </div>
                 </div>
               </div>
-          <section className={`${manageSaleList.navigationIcon} flex_between`}>
-                { 
-                <div className={`${manageSaleList.inputPart} flex_left`}>
-                    <i
-                    onClick={() => {
-                      dispatch(openModal('sales'))
-                      dispatch(addSalesData({modifiedData:supplierData, totalSalesValue, totalSalesItem, totalPaid, totalDiscount, totalCash: totalCashValue, totalBank: totalBankValue, totalBkash: totalBkashValue, totalNogod: totalNogodValue}))
-                    }}
-                    title="print" className="uil uil-print"></i>
-                    <span>Total : {supplierData?.length} </span>
-                    <input value={query} type="text" name="" id="" onChange={(e) => setQuery(e.target.value)}/>
-                    <i onClick={() => setQuery('')}  className="uil uil-times"></i>
-                    <label htmlFor="">From: </label>
-                <input value={range?.from} type="date" name="" id="" onChange={(e) => setRange({...range, from: e.target.value})}/>
-                <label htmlFor="">To: </label>
-                <input value={range?.to} type="date" name="" id="" onChange={(e) => setRange({...range, to: e.target.value})}/>
-                <i onClick={() =>setRange({from:'', to:''})} className="uil uil-times"></i>
-                   
-                </div>
-                }
-                
-          </section>
+              <section className={`${manageSaleList.navigationIcon} flex_between`}>
+                    { 
+                    <div className={`${manageSaleList.inputPart} flex_left`}>
+                        <i
+                        onClick={() => {
+                          dispatch(openModal('sales'))
+                          dispatch(addSalesData({modifiedData:supplierData, totalSalesValue, totalSalesItem, totalPaid, totalDiscount, totalCash: totalCashValue, totalBank: totalBankValue, totalBkash: totalBkashValue, totalNogod: totalNogodValue}))
+                        }}
+                        title="print" className="uil uil-print"></i>
+                        <span>Total : {supplierData?.length} </span>
+                        <input value={query} type="text" name="" id="" onChange={(e) => setQuery(e.target.value)}/>
+                        <i onClick={() => setQuery('')}  className="uil uil-times"></i>
+                        <label htmlFor="">From: </label>
+                    <input value={range?.from} type="date" name="" id="" onChange={(e) => setRange({...range, from: e.target.value})}/>
+                    <label htmlFor="">To: </label>
+                    <input value={range?.to} type="date" name="" id="" onChange={(e) => setRange({...range, to: e.target.value})}/>
+                    <i onClick={() =>setRange({from:'', to:''})} className="uil uil-times"></i>
+                      
+                    </div>
+                    }
+                    
+              </section>
           <section className={`${manageSaleList.navigationIcon} only_flex`}>
           
                 
