75cc2fe978a6c2910f96f8311f805376b57b8db6
akilinjamam <akilinjamam@gmail.com>
2025-02-19 12:28:40 +0600

    file-updated

--- src/components/dashboard/dashbord_titlebar/DashboardTitleBar.jsx
+++ src/components/dashboard/dashbord_titlebar/DashboardTitleBar.jsx
@@ -8,28 +8,27 @@ import { useEffect } from 'react';
 const DashboardTitleBar = () => {
 
     const getToken = localStorage.getItem('user');
-
     const splitToken = getToken?.split(' ')[1];
     const getUser = decodeJwt(splitToken);
 
 
     const [view, setView] = useState(false);
     const [date, setDate] = useState(new Date());
-    const {location, navigate} = useHome();
-    
+    const { location, navigate } = useHome();
+
     const mappingRoutes = location.split('/').slice(1)
 
     const handleRoute = (index) => {
         const findNavigation = mappingRoutes?.slice(0, index).join('/');
         navigate(`/${findNavigation}`)
-     }
-     const formattedDate = useMemo(() => {
+    }
+    const formattedDate = useMemo(() => {
         const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const monthNames = [
             'January', 'February', 'March', 'April', 'May', 'June',
             'July', 'August', 'September', 'October', 'November', 'December'
         ];
-        
+
         return {
             day: daysOfWeek[date.getDay()],
             monthDate: date.getDate(),
@@ -55,30 +54,30 @@ const DashboardTitleBar = () => {
 
     return (
         <div className={`${dashboardTitle.main}`}>
-           <div className={`${dashboardTitle.titleBar} flex_center `}>
+            <div className={`${dashboardTitle.titleBar} flex_center `}>
                 <div className={`${dashboardTitle.titleBarContainer} flex_between`}>
-                     <div className={`${dashboardTitle.mappedRoute}`}>
-                     {
-                        mappingRoutes?.map((route, index) => <span onClick={() => handleRoute(index+1)} key={index}><i className="uil uil-angle-right"></i>  {route.toUpperCase().replace(/_/g, ' ')} </span> )
-                     }
-                     </div>
-                   
+                    <div className={`${dashboardTitle.mappedRoute}`}>
+                        {
+                            mappingRoutes?.map((route, index) => <span onClick={() => handleRoute(index + 1)} key={index}><i className="uil uil-angle-right"></i>  {route.toUpperCase().replace(/_/g, ' ')} </span>)
+                        }
+                    </div>
+
                     <div onClick={() => setView(!view)} className={`${dashboardTitle.mappedRoute}`}>
-                        <div style={{width:'370px', display:'flex', alignItems:'center', justifyContent:'space-between'}}>
-                        <p>{formattedDate.day}, {formattedDate.monthDate} {formattedDate.monthName} {formattedDate.year},{" "}
-                        {date.getHours() % 12 || 12}:{String(date.getMinutes()).padStart(2, "0")}:
-                        {String(date.getSeconds()).padStart(2, "0")} {date.getHours() >= 12 ? 'PM' : 'AM'}</p>
-                        <p><i style={{fontSize:"30px"}} className="uil uil-user-circle"></i></p>
+                        <div style={{ width: '370px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                            <p>{formattedDate.day}, {formattedDate.monthDate} {formattedDate.monthName} {formattedDate.year},{" "}
+                                {date.getHours() % 12 || 12}:{String(date.getMinutes()).padStart(2, "0")}:
+                                {String(date.getSeconds()).padStart(2, "0")} {date.getHours() >= 12 ? 'PM' : 'AM'}</p>
+                            <p><i style={{ fontSize: "30px" }} className="uil uil-user-circle"></i></p>
                         </div>
-                        <div style={{display: `${view ? 'block': 'none'}`}} className={`${dashboardTitle.userDetails}`}>
-                            <h4>{getUser?.username?.length >= 16 ? getUser?.username?.slice(0,16) + '...' : getUser?.username}</h4>
+                        <div style={{ display: `${view ? 'block' : 'none'}` }} className={`${dashboardTitle.userDetails}`}>
+                            <h4>{getUser?.username?.length >= 16 ? getUser?.username?.slice(0, 16) + '...' : getUser?.username}</h4>
                             <hr />
                             <br />
                             <p onClick={handleLogOut}><i className="uil uil-sign-out-alt"></i> Logout</p>
                         </div>
                     </div>
                 </div>
-            </div>  
+            </div>
         </div>
     );
 };
