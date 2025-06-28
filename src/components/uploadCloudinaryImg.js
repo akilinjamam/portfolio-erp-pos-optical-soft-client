3d89ae22032e743483ce395da534cf29a2e3a44b
akilinjamam <akilinjamam@gmail.com>
2025-06-28 19:52:31 +0600

    cloudinary-environment-updated

--- src/components/uploadCloudinaryImg.js
+++ src/components/uploadCloudinaryImg.js
@@ -1,15 +1,16 @@
 import { toast } from "react-toastify";
-
+const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+const cloudName = import.meta.env.VITE_UPLOAD_CLOUD_NAME;
 export const updloadCloudinaryImage = (imgFile, setImgHolder, setUploading) => {
 
     const data = new FormData();
     data.append("file", imgFile);
-    data.append("upload_preset", "smipgehv");
-    data.append("cloud_name", "dw57lx7qa");
+    data.append("upload_preset", `${uploadPreset}`);
+    data.append("cloud_name", `${cloudName}`);
 
     setUploading(true)
 
-    fetch("https://api.cloudinary.com/v1_1/dw57lx7qa/image/upload", {
+    fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
         method: "post",
         body: data
     })
