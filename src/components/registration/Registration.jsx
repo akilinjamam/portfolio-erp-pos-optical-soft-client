e0ab4fa247f12be2d0c4abb825aa7f3c510905a6
akilinjamam <akilinjamam@gmail.com>
2026-01-22 10:19:09 +0600

    file-updated

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -95,11 +95,6 @@ const Registration = () => {
                     </form>
                 </div>
             </div>
-            <div className={`${signup.displayWidthLimitControll}`}>
-                <div className="flex_center">
-                    <p>Screen Size must have to be more than 1242px</p>
-                </div>
-            </div>
         </div>
     );
 };
